bf8e607de9024c8865d00b703d141403bf2419f8
mystardev developer <mystargithub@gmail.com>
2021-06-17 11:18:24 +0700

    add graph

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -174,20 +174,20 @@ demo = {
       },
       responsive: true,
       scales: {
-        yAxes: [{
-          barPercentage: 1.6,
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.0)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            suggestedMin: 0,
-            suggestedMax: 125,
-            padding: 20,
-            fontColor: "#9a9a9a"
-          }
-        }],
+        // yAxes: [{
+        //   barPercentage: 1.6,
+        //   gridLines: {
+        //     drawBorder: false,
+        //     color: 'rgba(29,140,248,0.0)',
+        //     zeroLineColor: "transparent",
+        //   },
+        //   ticks: {
+        //     suggestedMin: 0,
+        //     suggestedMax: 125,
+        //     padding: 20,
+        //     fontColor: "#9a9a9a"
+        //   }
+        // }],
 
         xAxes: [{
           barPercentage: 1.6,
@@ -270,20 +270,20 @@ demo = {
       },
       responsive: true,
       scales: {
-        yAxes: [{
-          barPercentage: 1.6,
-          gridLines: {
-            drawBorder: false,
-            color: 'rgba(29,140,248,0.0)',
-            zeroLineColor: "transparent",
-          },
-          ticks: {
-            suggestedMin: 50,
-            suggestedMax: 125,
-            padding: 20,
-            fontColor: "#9e9e9e"
-          }
-        }],
+        // yAxes: [{
+        //   barPercentage: 1.6,
+        //   gridLines: {
+        //     drawBorder: false,
+        //     color: 'rgba(29,140,248,0.0)',
+        //     zeroLineColor: "transparent",
+        //   },
+        //   ticks: {
+        //     suggestedMin: 50,
+        //     suggestedMax: 125,
+        //     padding: 20,
+        //     fontColor: "#9e9e9e"
+        //   }
+        // }],
 
         xAxes: [{
           barPercentage: 1.6,
@@ -374,7 +374,7 @@ demo = {
         pointHoverRadius: 4,
         pointHoverBorderWidth: 15,
         pointRadius: 4,
-        data: [5, 5, 3, 2, 5, 4],
+        data: [1, 0, 1, 0, 0],
       }]
     };
 
@@ -417,14 +417,14 @@ demo = {
     var myChart = new Chart(ctxGreen, {
       type: 'line',
       data: data,
-      // options: gradientChartOptionsConfigurationWithTooltipGreen
+      options: gradientChartOptionsConfigurationWithTooltipGreen
 
     });
 
 
 
-    var chart_labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG'];
-    var chart_data = [40, 70, 90, 70, 85, 60, 75, 80];
+    var chart_labels = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN'];
+    var chart_data = [30, 25, 29, 26, 28, 30];
 
 
     var ctx = document.getElementById("chartBig1").getContext('2d');
@@ -466,7 +466,7 @@ demo = {
       myChartData.update();
     });
     $("#1").click(function() {
-      var chart_data = [80, 120, 105, 110, 95, 105, 90, 100];
+      var chart_data = [28, 25, 29, 27, 25, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
@@ -474,42 +474,42 @@ demo = {
     });
 
     $("#2").click(function() {
-      var chart_data = [60, 80, 65, 130, 80, 105, 90, 130];
+      var chart_data = [30, 25, 29, 27, 25, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
       myChartData.update();
     });
     $("#3").click(function() {
-      var chart_data = [70, 10, 65, 100, 80, 105, 90, 120];
+      var chart_data = [27, 29, 25, 28, 26, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
       myChartData.update();
     });
     $("#4").click(function() {
-      var chart_data = [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120];
+      var chart_data = [29, 25, 29, 26, 28, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
       myChartData.update();
     });
     $("#5").click(function() {
-      var chart_data = [60, 80, 65, 130, 80, 105, 90, 130, 70, 115, 60, 130];
+      var chart_data = [27, 29, 25, 28, 26, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
       myChartData.update();
     });
     $("#6").click(function() {
-      var chart_data = [50, 70, 55, 90, 60, 65, 60, 80, 70, 105, 80, 110];
+      var chart_data = [25, 27, 24, 28, 25, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
       myChartData.update();
     });
     $("#7").click(function() {
-      var chart_data = [80, 120, 105, 110, 95, 105, 90, 100, 80, 95, 70, 120];
+      var chart_data = [28, 25, 29, 26, 28, 30];
       var data = myChartData.config.data;
       data.datasets[0].data = chart_data;
       data.labels = chart_labels;
@@ -543,7 +543,7 @@ demo = {
           borderWidth: 2,
           borderDash: [],
           borderDashOffset: 0.0,
-          data: [10, 20, 10, 80, 100, 45],
+          data: [30, 20, 10, 80, 100, 45],
         }]
       },
       options: gradientBarChartConfiguration
